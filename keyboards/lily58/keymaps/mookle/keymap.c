eb4686773183859d8d585409eedba7dd8eba8ef8
mookle <aaf4c@waddi.love>
2023-10-23 23:16:15 +0100

    Linux-related updates [lily58]:
    - Remove QWERTY and TEST layers
    - Use standard (non-mac) UK layout

--- keyboards/lily58/keymaps/mookle/keymap.c
+++ keyboards/lily58/keymaps/mookle/keymap.c
@@ -2,12 +2,10 @@
 
 enum layer_number {
   _COLEMAK = 0,
-  _QWERTY,
   _LOWER,
   _RAISE,
   _COMBO,
   _NVIM,
-  _TEST,
 };
 
 enum custom_keycodes {
@@ -40,9 +38,7 @@ enum custom_keycodes {
 #define LOWER  MO(_LOWER)
 #define RAISE  MO(_RAISE)
 #define COMBO  MO(_COMBO)
-#define QWERTY TG(_QWERTY)
 #define NVIM   MO(_NVIM)
-#define TEST   TG(_TEST)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_COLEMAK] = LAYOUT(
@@ -53,35 +49,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
      KC_BSPC ,  KC_A   ,  KC_R   ,  KC_S   ,   KC_T,    KC_G   ,                         KC_M   ,  KC_N   ,  KC_E   ,  KC_I   ,  KC_O   , KC_ENT  ,
 // |---------|---------|---------|---------|---------|---------|---------|  |---------|---------|---------|---------|---------|---------|---------|
-     KC_LSFT ,  KC_Z   ,  KC_X   ,  KC_C   ,   KC_D,    KC_V   ,  NVIM   ,     TEST   ,  KC_K   ,  KC_H   , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT ,
+     KC_LSFT ,  KC_Z   ,  KC_X   ,  KC_C   ,   KC_D,    KC_V   , XXXXXXX ,    XXXXXXX ,  KC_K   ,  KC_H   , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT ,
 // |---------|---------|---------|---------|---------|---------|---------|  |---------|---------|---------|---------|---------|---------|---------|
                                    KC_LCTL , KC_LGUI ,  LOWER  , KC_SPC  ,    KC_SPC  ,  RAISE  , KC_RGUI , KC_RALT
-//                               |---------|---------|---------|---------|  |---------|---------|---------|---------|
-),
-[_TEST] = LAYOUT(
-// |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
-     _______ , _______ , _______ , _______ , _______ , _______ ,                        _______ , _______ , _______ , _______ , _______ , _______ ,
-// |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
-     _______ , _______ , _______ , _______ , _______ , _______ ,                        _______ , _______ , _______ , _______ , _______ , _______ ,
-// |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
-     _______ , _______ , _______ , _______ , _______ , _______ ,                        _______ , _______ , _______ , _______ , _______ , KC_TAB  ,
-// |---------|---------|---------|---------|---------|---------|---------|  |---------|---------|---------|---------|---------|---------|---------|
-     _______ , _______ , _______ , _______ , _______ , _______ , _______ ,    _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
-// |---------|---------|---------|---------|---------|---------|---------|  |---------|---------|---------|---------|---------|---------|---------|
-                                   _______ , _______ , _______ , _______ ,    KC_ENT  , _______ , _______ , _______
-//                               |---------|---------|---------|---------|  |---------|---------|---------|---------|
-),
-[_QWERTY] = LAYOUT(
-// |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
-     _______ , _______ , _______ , _______ , _______ , _______ ,                        _______ , _______ , _______ , _______ , _______ , KC_BSPC ,
-// |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
-     _______ , _______ , _______ ,  KC_E   ,  KC_R   ,  KC_T   ,                         KC_Y   ,  KC_U   ,  KC_I   ,  KC_O   ,  KC_P   , _______ ,
-// |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
-     KC_LSFT , _______ ,  KC_S   ,  KC_D   ,  KC_F   ,  KC_G   ,                         KC_H   ,  KC_J   ,  KC_K   ,  KC_L   , KC_SCLN , _______ ,
-// |---------|---------|---------|---------|---------|---------|---------|  |---------|---------|---------|---------|---------|---------|---------|
-     KC_LCTL , _______ , _______ , _______ ,  KC_V   ,  KC_B   , _______ ,    _______ ,  KC_N   ,  KC_M   , _______ , _______ , _______ , KC_LALT ,
-// |---------|---------|---------|---------|---------|---------|---------|  |---------|---------|---------|---------|---------|---------|---------|
-                                   _______ , _______ , _______ , _______ ,    _______,  _______ , _______ , KC_GRV
 //                               |---------|---------|---------|---------|  |---------|---------|---------|---------|
 ),
 [_NVIM] = LAYOUT(
@@ -99,13 +69,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 [_LOWER] = LAYOUT(
 // |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
-     KC_GRV  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                         KC_F7  ,  KC_F8  ,  KC_F9  , KC_F10  , KC_F11  , KC_F12  ,
+     KC_GRV  , XXXXXXX , XXXXXXX , KC_NUHS , XXXXXXX , XXXXXXX ,                         KC_F7  ,  KC_F8  ,  KC_F9  , KC_F10  , KC_F11  , KC_F12  ,
 // |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
      XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                        XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
 // |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
      XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                        XXXXXXX , KC_LEFT , KC_DOWN ,  KC_UP  , KC_RGHT , XXXXXXX ,
 // |---------|---------|---------|---------|---------|---------|---------|  |---------|---------|---------|---------|---------|---------|---------|
-     _______ ,KC_TILDE , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,    XXXXXXX , KC_CIRC , KC_AMPR , KC_ASTR , KC_PIPE , KC_BSLS , _______ ,
+     _______ ,KC_TILDE , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,    XXXXXXX , KC_CIRC , KC_AMPR , KC_ASTR , KC_PIPE , KC_NUBS , _______ ,
 // |---------|---------|---------|---------|---------|---------|---------|  |---------|---------|---------|---------|---------|---------|---------|
                                    _______ , _______ , _______ , _______ ,    KC_UNDS ,  COMBO  , _______ , _______
 //                               |---------|---------|---------|---------|  |---------|---------|---------|---------|
@@ -118,14 +88,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
      KC_LCBR , KC_LPRN , KC_LBRC , KC_RBRC , KC_RPRN , KC_RCBR ,                        N_HSPLT , C(KC_N) , C(KC_E) , C(KC_I) , C(KC_O) , XXXXXXX ,
 // |---------|---------|---------|---------|---------|---------|---------|  |---------|---------|---------|---------|---------|---------|---------|
-     _______ , KC_EXLM ,  KC_AT  , S(KC_3) , KC_DLR  , KC_PERC , XXXXXXX ,    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , _______ ,
+     _______ , KC_EXLM ,S(KC_QUOT),S(KC_3) , KC_DLR  , KC_PERC , XXXXXXX ,    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , _______ ,
 // |---------|---------|---------|---------|---------|---------|---------|  |---------|---------|---------|---------|---------|---------|---------|
                                    _______ , _______ ,  COMBO  , KC_MINS ,    _______ , _______ , _______ , _______
 //                               |---------|---------|---------|---------|  |---------|---------|---------|---------|
 ),
 [_COMBO] = LAYOUT(
 // |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
-     XXXXXXX , QWERTY  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                        XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                        XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
 // |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
      XXXXXXX , T_CLOSE , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                        XXXXXXX , XXXXXXX , T_VSPLT , XXXXXXX , XXXXXXX , XXXXXXX ,
 // |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
@@ -142,51 +112,32 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _COMBO);
 }
 
-//SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
-#ifdef OLED_ENABLE
-
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_keyboard_master())
-    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-  return rotation;
-}
-
-// When you add source files to SRC in rules.mk, you can use functions.
-const char *read_layer_state(void);
-const char *read_logo(void);
-void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
-
-// const char *read_mode_icon(bool swap);
-// const char *read_host_led_state(void);
-// void set_timelog(void);
-// const char *read_timelog(void);
-
-bool oled_task_user(void) {
-  if (is_keyboard_master()) {
-    // If you want to change the display of OLED, you need to change here
-    oled_write_ln(read_layer_state(), false);
-    oled_write_ln(read_keylog(), false);
-    oled_write_ln(read_keylogs(), false);
-    //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
-    //oled_write_ln(read_host_led_state(), false);
-    //oled_write_ln(read_timelog(), false);
-  } else {
-    oled_write(read_logo(), false);
-  }
-    return false;
-}
-#endif // OLED_ENABLE
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-#ifdef OLED_ENABLE
-    set_keylog(keycode, record);
-#endif
-    // set_timelog();
-  }
   switch (keycode) {
+    case KC_2:
+      if (record->event.pressed) {
+        if (get_mods() && (MOD_BIT(KC_LSFT) || MOD_BIT(KC_RSFT))) {
+          register_code(KC_QUOT);
+        } else {
+          register_code(KC_2);
+        }
+      } else {
+        unregister_code(KC_QUOT);
+        unregister_code(KC_2);
+      }
+      return false; // skip all further processing of this key
+    case KC_QUOT:
+      if (record->event.pressed) {
+        if (get_mods() && (MOD_BIT(KC_LSFT) || MOD_BIT(KC_RSFT))) {
+          register_code(KC_2);
+        } else {
+          register_code(KC_QUOT);
+        }
+      } else {
+        unregister_code(KC_2);
+        unregister_code(KC_QUOT);
+      }
+      return false; // skip all further processing of this key
     case T_PANE:
       if (record->event.pressed) {
         SEND_STRING(SS_LCTL("b"));
--- keyboards/lily58/keymaps/mookle/rules.mk
+++ keyboards/lily58/keymaps/mookle/rules.mk
@@ -6,9 +6,10 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = no		# Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_ENABLE= yes     # OLED display
+OLED_ENABLE = yes			# OLED display
+BOOTLOADER = atmel-dfu		# Needed for the EliteC controller
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
