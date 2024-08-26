6e6fbdf9b315bb54ac895da23aa274cb04bcf282
mookle <aaf4c@waddi.love>
2024-08-26 15:24:03 +0100

    Macro tidy-up:
    - Remove deprecated tmux/nvim navigation
    - Use ctrl+h/j/k/l for nvim navigation. Avoids ctrl+i (tab keycode) issues in shell
    - Increase keystroke timeout to 150ms for `§` char

--- keyboards/lily58/keymaps/mookle/keymap.c
+++ keyboards/lily58/keymaps/mookle/keymap.c
@@ -6,7 +6,6 @@ enum layer_number {
   _LOWER,
   _RAISE,
   _COMBO,
-  _NVIM,
 };
 
 enum custom_keycodes {
@@ -15,20 +14,12 @@ enum custom_keycodes {
   T_CLOSE,
   T_PREV,
   T_NEXT,
-  T_UP,
-  T_DOWN,
-  T_LEFT,
-  T_RIGHT,
   T_VSPLT,
   T_HSPLT,
   T_ZOOM,
   // Neovim macro triggers
   N_SAVE,
   N_CLOSE,
-  N_UP,
-  N_DOWN,
-  N_LEFT,
-  N_RIGHT,
   N_VSPLT,
   N_HSPLT,
   N_VOPEN,
@@ -40,7 +31,6 @@ enum custom_keycodes {
 #define LOWER   MO(_LOWER)
 #define RAISE   MO(_RAISE)
 #define COMBO   MO(_COMBO)
-#define NVIM    MO(_NVIM)
 #define COLEMAK DF(_COLEMAK)
 #define QWERTY  DF(_QWERTY)
 
@@ -71,19 +61,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    KC_LCTL , KC_LGUI ,  LOWER  , KC_SPC  ,    KC_SPC  ,  RAISE  , KC_RGUI , KC_RALT
 //                               |---------|---------|---------|---------|  |---------|---------|---------|---------|
 ),
-[_NVIM] = LAYOUT(
-// |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
-     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                        XXXXXXX , XXXXXXX , N_VOPEN , XXXXXXX , XXXXXXX , XXXXXXX ,
-// |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
-     XXXXXXX , N_CLOSE , N_SAVE  , XXXXXXX , XXXXXXX , XXXXXXX ,                        N_HOPEN , XXXXXXX , N_VSPLT , XXXXXXX , XXXXXXX , XXXXXXX ,
-// |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
-     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                        N_HSPLT , C(KC_N) , C(KC_E) , C(KC_I) , C(KC_O) , XXXXXXX ,
-// |---------|---------|---------|---------|---------|---------|---------|  |---------|---------|---------|---------|---------|---------|---------|
-     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , _______ ,    _______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
-// |---------|---------|---------|---------|---------|---------|---------|  |---------|---------|---------|---------|---------|---------|---------|
-                                   XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX
-//                               |---------|---------|---------|---------|  |---------|---------|---------|---------|
-),
 [_LOWER] = LAYOUT(
 // |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
      KC_GRV  , XXXXXXX , XXXXXXX , KC_NUHS , XXXXXXX , XXXXXXX ,                         KC_F7  ,  KC_F8  ,  KC_F9  , KC_F10  , KC_F11  , KC_F12  ,
@@ -103,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
      XXXXXXX , N_CLOSE , N_SAVE  , XXXXXXX , XXXXXXX , XXXXXXX ,                        N_HOPEN , XXXXXXX , N_VSPLT , XXXXXXX , XXXXXXX , XXXXXXX ,
 // |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
-     KC_LCBR , KC_LPRN , KC_LBRC , KC_RBRC , KC_RPRN , KC_RCBR ,                        N_HSPLT , C(KC_N) , C(KC_E) , C(KC_I) , C(KC_O) , XXXXXXX ,
+     KC_LCBR , KC_LPRN , KC_LBRC , KC_RBRC , KC_RPRN , KC_RCBR ,                        N_HSPLT , C(KC_H) , C(KC_J) , C(KC_K) , C(KC_L) , XXXXXXX ,
 // |---------|---------|---------|---------|---------|---------|---------|  |---------|---------|---------|---------|---------|---------|---------|
      _______ , KC_EXLM ,S(KC_QUOT),S(KC_3) , KC_DLR  , KC_PERC , KC_MPLY ,    KC_MSTP , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , _______ ,
 // |---------|---------|---------|---------|---------|---------|---------|  |---------|---------|---------|---------|---------|---------|---------|
@@ -114,20 +91,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
      XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                        XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
 // |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
-     XXXXXXX , T_CLOSE , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                        XXXXXXX , XXXXXXX , T_VSPLT , XXXXXXX , XXXXXXX , XXXXXXX ,
+     XXXXXXX , T_CLOSE , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                        XXXXXXX , T_PREV  , T_VSPLT , T_NEXT  , XXXXXXX , XXXXXXX ,
 // |---------|---------|---------|---------|---------|---------|                      |---------|---------|---------|---------|---------|---------|
-     XXXXXXX , T_PREV  , XXXXXXX , T_ZOOM  , T_NEXT  , T_PANE  ,                        T_HSPLT , T_LEFT  , T_DOWN  ,  T_UP   , T_RIGHT , XXXXXXX ,
+     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , T_PANE  ,                        T_HSPLT , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
 // |---------|---------|---------|---------|---------|---------|---------|  |---------|---------|---------|---------|---------|---------|---------|
-     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , _______ ,    _______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+     XXXXXXX , T_ZOOM  , XXXXXXX , XXXXXXX , XXXXXXX , KC_X , _______ ,    _______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
 // |---------|---------|---------|---------|---------|---------|---------|  |---------|---------|---------|---------|---------|---------|---------|
                                    _______ , _______ , _______ , _______ ,    _______ , _______ , _______ , _______
 //                               |---------|---------|---------|---------|  |---------|---------|---------|---------|
 )
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _COMBO);
-}
+/*layer_state_t layer_state_set_user(layer_state_t state) {*/
+/*  return update_tri_layer_state(state, _LOWER, _RAISE, _COMBO);*/
+/*}*/
 
 const key_override_t at_dquote_override = ko_make_basic(MOD_MASK_SHIFT, KC_2, S(KC_QUOT));
 const key_override_t dquote_at_override = ko_make_basic(MOD_MASK_SHIFT, KC_QUOT, S(KC_2));
@@ -145,7 +122,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         SEND_STRING(SS_DOWN(X_LSFT) SS_DOWN(X_LCTL));
         SEND_STRING(SS_TAP(X_U));
         SEND_STRING(SS_UP(X_LSFT) SS_UP(X_LCTL));
-        SEND_STRING(SS_DELAY(100) "00A7" SS_TAP(X_ENTER));
+        SEND_STRING(SS_DELAY(150) "00A7" SS_TAP(X_ENTER));
         // If not for the SS_DELAY (because kitty unicode_input) then this
         // one-liner would do the job:
         //send_unicode_string("§");
@@ -153,92 +130,37 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case T_PANE:
       if (record->event.pressed) {
-        SEND_STRING(SS_LCTL("b"));
-        SEND_STRING(SS_TAP(X_C));
+        SEND_STRING(SS_LCTL("b") "c");
       }
       break;
     case T_CLOSE:
       if (record->event.pressed) {
-        SEND_STRING(SS_LCTL("b"));
-        SEND_STRING(SS_TAP(X_X));
+        SEND_STRING(SS_LCTL("b") "xy");
       }
       break;
     case T_PREV:
       if (record->event.pressed) {
-        SEND_STRING(SS_LCTL("b"));
-        SEND_STRING(SS_TAP(X_P));
+        SEND_STRING(SS_LCTL("b") "p");
       }
       break;
     case T_NEXT:
       if (record->event.pressed) {
-        SEND_STRING(SS_LCTL("b"));
-        SEND_STRING(SS_TAP(X_N));
-      }
-      break;
-    case T_UP:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LCTL("b"));
-        SEND_STRING(SS_TAP(X_UP));
-      }
-      break;
-    case T_DOWN:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LCTL("b"));
-        SEND_STRING(SS_TAP(X_DOWN));
-      }
-      break;
-    case T_LEFT:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LCTL("b"));
-        SEND_STRING(SS_TAP(X_LEFT));
-      }
-      break;
-    case T_RIGHT:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LCTL("b"));
-        SEND_STRING(SS_TAP(X_RIGHT));
+        SEND_STRING(SS_LCTL("b") "n");
       }
       break;
     case T_VSPLT:
       if (record->event.pressed) {
-        SEND_STRING(SS_LCTL("b"));
-        SEND_STRING(SS_LSFT("5"));
+        SEND_STRING(SS_LCTL("b") ":splitw -h" SS_TAP(X_ENTER));
       }
       break;
     case T_HSPLT:
       if (record->event.pressed) {
-        SEND_STRING(SS_LCTL("b"));
-        SEND_STRING(SS_LSFT("'"));
+        SEND_STRING(SS_LCTL("b") ":splitw" SS_TAP(X_ENTER));
       }
       break;
     case T_ZOOM:
       if (record->event.pressed) {
-        SEND_STRING(SS_LCTL("b"));
-        SEND_STRING(SS_TAP(X_Z));
-      }
-      break;
-    case N_UP:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LCTL("w"));
-        SEND_STRING(SS_TAP(X_UP));
-      }
-      break;
-    case N_DOWN:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LCTL("w"));
-        SEND_STRING(SS_TAP(X_DOWN));
-      }
-      break;
-    case N_LEFT:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LCTL("w"));
-        SEND_STRING(SS_TAP(X_LEFT));
-      }
-      break;
-    case N_RIGHT:
-     if (record->event.pressed) {
-        SEND_STRING(SS_LCTL("w"));
-        SEND_STRING(SS_TAP(X_RIGHT));
+        SEND_STRING(SS_LCTL("b") "z");
       }
       break;
     case N_HOPEN:
@@ -253,22 +175,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case N_VSPLT:
       if (record->event.pressed) {
-        SEND_STRING(":vsp"SS_TAP(X_ENTER));
+        SEND_STRING(":vsp" SS_TAP(X_ENTER));
       }
       break;
     case N_HSPLT:
       if (record->event.pressed) {
-        SEND_STRING(":sp"SS_TAP(X_ENTER));
+        SEND_STRING(":sp" SS_TAP(X_ENTER));
       }
       break;
     case N_CLOSE:
       if (record->event.pressed) {
-        SEND_STRING(":q"SS_TAP(X_ENTER));
+        SEND_STRING(":q" SS_TAP(X_ENTER));
       }
       break;
     case N_SAVE:
       if (record->event.pressed) {
-        SEND_STRING(":w"SS_TAP(X_ENTER));
+        SEND_STRING(":w" SS_TAP(X_ENTER));
       }
       break;
   }
